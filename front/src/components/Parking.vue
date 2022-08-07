8ede8015a3687b3ff7e41e518d8c9a5ec032b8f4
Thibaut Derodit <fubautsu@gmail.com>
2022-08-07 15:11:45 +0200

    Ajout parking relais

--- front/src/components/Parking.vue
+++ front/src/components/Parking.vue
@@ -2,7 +2,50 @@
   <div class="q-pa-md">
     <h1 class="text-center">O&ugrave; se garer ?</h1>
     <section>
-      <h3>Mairie et église</h3>
+        <h3>Parkings relais + Tram</h3>
+        <p>
+            Le réseau de transports en commun de Clermont-Ferrand est gratuit le week-end et les jours fériés. Les parkings relais et de proximité sont donc tous gratuit.
+            Le station de tram desservant la mairie de Clermont-Ferrand est l'arrêt "Hôtel de ville".
+        </p>
+        <p>Nous vous encourageons fortement à privilégier cette solution.</p>
+        <div class="row">
+            <div class="col">
+                <iframe src="https://www.google.com/maps/d/u/0/embed?mid=1TPprSYwCjRVckl-SxvMMEgsa0fogx3o&ehbc=2E312F" :width="iframe" height="480"></iframe>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
+                <p class="text-h6 text-center">Direction La Pardieu - Gare</p>
+                <q-list>
+                    <q-item v-for="(item, index) in pardieu.values">
+                        <q-item-section avatar>
+                            <q-avatar :size="size" :color="pardieu.color" :text-color="pardieu.text">{{ index + 1 }}</q-avatar>
+                        </q-item-section>
+                        <q-item-section>
+                            <q-item-label>{{ item.nom }}</q-item-label>
+                            <q-item-label caption>Arrêt : {{ item.arret }} - Temps de trajet : {{ item.duration }}min - Places : {{ item.place }}</q-item-label>
+                        </q-item-section>
+                    </q-item>
+                </q-list>
+            </div>
+            <div class="col">
+                <p class="text-h6 text-center">Direction Les Vergnes</p>
+                <q-list>
+                    <q-item v-for="(item, index) in vergnes.values">
+                        <q-item-section avatar>
+                            <q-avatar :size="size" :color="vergnes.color" :text-color="vergnes.text">{{ index + 1 }}</q-avatar>
+                        </q-item-section>
+                        <q-item-section>
+                            <q-item-label>{{ item.nom }}</q-item-label>
+                            <q-item-label caption>Arrêt : {{ item.arret }} - Temps de trajet : {{ item.duration }}min - Places : {{ item.place }}</q-item-label>
+                        </q-item-section>
+                    </q-item>
+                </q-list>
+            </div>
+        </div>
+    </section>
+    <section>
+      <h3>Parkings autour de la Mairie et de l'église</h3>
       <div ref="mapContainer" class="row">
         <div class="col">
           <iframe src="https://www.google.com/maps/d/u/0/embed?mid=15X2bPnMotmziUOHqkCDrJzeuQNBF7C4&ehbc=2E312F" :width="iframe" height="480"></iframe>
@@ -10,27 +53,30 @@
       </div>
       <div class="row">
         <div class="col">
-            <h6 class="text-center">Parking en surface</h6>
+            <p class="text-h6 text-center">Parking en surface</p>
           <q-list>
             <q-item v-for="(item, index) in surface.values">
                 <q-item-section avatar>
                     <q-avatar :size="size" :color="surface.color" :text-color="surface.text">{{ index + 1 }}</q-avatar>
                 </q-item-section>
                 <q-item-section>
-                    <q-item-label>{{ item }}</q-item-label>
+                    <q-item-label>{{ item.text }}</q-item-label>
+                    <q-item-label caption>{{ item.caption }}</q-item-label>
                 </q-item-section>
             </q-item>
           </q-list>
+          <p class="text-caption">Il est possible de payer le stationnement de surface via l'application PrestoPark</p>
         </div>
         <div class="col">
-            <h6 class="text-center">Parking en surface</h6>
+            <p class="text-h6 text-center">Parking en surface</p>
             <q-list>
                 <q-item v-for="(item, index) in underground.values">
                     <q-item-section avatar>
                         <q-avatar :size="size" :color="underground.color" :text-color="underground.text">{{ index + 1 }}</q-avatar>
                     </q-item-section>
                     <q-item-section>
-                        <q-item-label>{{ item }}</q-item-label>
+                        <q-item-label>{{ item.text }}</q-item-label>
+                    <q-item-label caption>{{ item.caption }}</q-item-label>
                     </q-item-section>
                 </q-item>
             </q-list>
@@ -40,13 +86,6 @@
     </section>
   </div>
 </template>
-<style scoped>
-#map {
-  width: 400px;
-  height: 400px;
-  border: 1px solid red;
-}
-</style>
 <script>
 import { ref } from "vue";
 
@@ -62,25 +101,42 @@ export default {
     return {
         iframe: 0,
         size: "md",
+        pardieu: {
+            color: "blue-10",
+            text: "white",
+            values: [
+                { nom: "Henri Dunant", arret: "CHU G. Montpied", place: "550", duration: "11"},
+                { nom: "Margeride", arret: "Margeride", place: "110", duration: "21"},
+                { nom: "La Pardieu", arret: "La Pardieu Gare", place: "120", duration: "25"},
+            ],
+        },
+        vergnes: {
+            color: "light-green-5",
+            text: "white",
+            values: [
+                { nom: "Les Pistes", arret: "Les Pistes", place: "400", duration: "14"}
+            ],
+        },
         surface: {
             color: "red",
             text: "white",
             values: [
-                "Mairie",
-                "Ecole Edgar Quinet",
-                "Place des Bughes",
-                "Palais de Justice"
+                { text: "Mairie", caption: "1.50€ la 1ère heure. 3.00€ les 2 heures" },
+                { text: "Ecole Edgar Quinet", caption: "0.90€ la 1ère heure. 1.80€ les 2 heures" },
+                { text: "Place des Bughes", caption: "0.50€ la 1ère heure. 0.60€ les 2 heures" },
+                { text: "Palais de Justice", caption: "1.50€ la 1ère heure. 3.00€ les 2 heures" },
+                { text: "Place d'Espagne", caption: "0.90€ la 1ère heure. 1.80€ les 2 heures" },
             ]
         },
         underground: {
             color: "yellow",
             text: "white",
             values: [
-                "Cathédrale",
-                "Blaise Pascal",
-                "Vercingetorix",
-                "Centre Jaude",
-                "Saint-Pierre"
+                { text: "Cathédrale", caption: "1.40€ la 1ère heure. 3.10€ les 2 heures" },
+                { text: "Blaise Pascal", caption: "1.50€ la 1ère heure. 3.10€ les 2 heures" },
+                { text: "Vercingetorix", caption: "1.50€ la 1ère heure. 3.10€ les 2 heures" },
+                { text: "Centre Jaude", caption: "1.50€ la 1ère heure. 3.10€ les 2 heures" },
+                { text: "Saint-Pierre", caption: "1.50€ la 1ère heure. 3.10€ les 2 heures" }
             ]
         }
     }
