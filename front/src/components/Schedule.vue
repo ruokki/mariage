eb2056ada44b58750817fb81e7336bd15f43cc5f
Thibaut Derodit <fubautsu@gmail.com>
2022-11-27 13:00:59 +0100

    Ajout texte pour la partie "Déroulé

--- front/src/components/Schedule.vue
+++ front/src/components/Schedule.vue
@@ -10,18 +10,25 @@
             <p class="location"><q-icon name="place" size="15px" /> Mairie de Clermont-Ferrand, 10 Rue Philippe Marcombes, 63000 Clermont Ferrand</p>
         </template>
         <div>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          On vous attend à la mairie de Clermont-Ferrand pour débuter la journée la plus importante de notre vie ! La cérémonie débute à 14h00, nous vous attendons donc à partir de 13h00 afin de permettre à ceux qui veulent
+          y assister d'entrer dans la salle des cérémonies. Nous vous recommandons de venir en tram. La mairie étant située en plein centre ville, le stationnement peut s'avérer compliqué.
+          <br />
+          Mais dieu merci, il y a un arrêt de tram au pied de la mairie. C'est l'arrêt "Hôtel de ville".
         </div>
       </q-timeline-entry>
       <q-timeline-entry
-        subtitle="16h00"
+        subtitle="15h00"
       >
         <template v-slot:title>
             Cérémonie à l'église
             <p class="location"><q-icon name="place" size="15px" /> Église Saint Eutrope, 1 Place Saint-Eutrope, 63000 Clermont-Ferrand</p>
         </template>
         <div>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          La bénédiction aura lieu à l'église Saint-Eutrope. Elle est située à 10min à pied de la mairie, il n'y aura donc pas besoin d'utiliser la voiture pour y aller. 
+          Elle sera officiée par le père Noël (le vrai).
+          <p>
+            Pour les personnes à mobilité réduite, nous prévoyons une navette afin de vous amener de la mairie à l'église (Clermont-Ferrand étant un peu valloné).
+          </p>
         </div>
       </q-timeline-entry>
       <q-timeline-entry
@@ -32,7 +39,7 @@
             <p class="location"><q-icon name="place" size="15px" /> Salle "L'Astragale", 63800 Cournon-d'Auvergne</p>
         </template>
         <div>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          Une fois toutes les cérémonies passées, nous nous retrouverons tous à Cournon-d'Auvergne afin de fêter comme il se doit cette grande étape de notre vie !
         </div>
       </q-timeline-entry>
     </q-timeline>
