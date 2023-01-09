77721e445e10ca74951f6900351de99a7f3fbfe7
Thibaut Derodit <fubautsu@gmail.com>
2023-01-09 14:02:53 +0100

    Amélioration visibilité mobile timer

--- front/src/components/Timer.vue
+++ front/src/components/Timer.vue
@@ -1,7 +1,101 @@
 <template>
   <div class="q-pa-md">
-    <h1 class="text-center">15 JUILLET 2023</h1>
-    <div class="row">
+    <!-- Desktop -->
+    <div class="md">
+      <h1 class="text-center">15 JUILLET 2023</h1>
+      <div class="row">
+        <div class="col" ref="myCol">
+          <q-circular-progress
+            :value="month"
+            :size="cardSize"
+            show-value
+            :thickness="thick"
+            :color="color"
+            :center-color="center"
+            :track-color="track"
+            :square="true"
+            class="q-ma-md"
+            :bordered="false"
+            :min="0"
+            :max="12"
+          />
+          <h2 class="text-center">Mois</h2>
+        </div>
+        <div class="col">
+          <q-circular-progress
+            :value="days"
+            :size="cardSize"
+            show-value
+            :thickness="thick"
+            :square="true"
+            :color="color"
+            :center-color="center"
+            :track-color="track"
+            class="q-ma-md"
+            :bordered="false"
+            :min="0"
+            :max="500"
+          />
+          <h2 class="text-center">Jours</h2>
+        </div>
+        <div class="col">
+          <q-circular-progress
+            :value="hours"
+            :size="cardSize"
+            show-value
+            :thickness="thick"
+            :color="color"
+            :center-color="center"
+            :track-color="track"
+            :square="true"
+            class="q-ma-md"
+            :bordered="false"
+            :min="0"
+            :max="24"
+          />
+          <h2 class="text-center">Heures</h2>
+        </div>
+        <div class="col">
+          <q-circular-progress
+            :value="minutes"
+            :size="cardSize"
+            show-value
+            :thickness="thick"
+            :color="color"
+            :center-color="center"
+            :track-color="track"
+            :square="true"
+            class="q-ma-md"
+            :bordered="false"
+            :min="0"
+            :max="60"
+          />
+          <h2 class="text-center">Minutes</h2>
+        </div>
+        <div class="col">
+          <q-circular-progress
+            :value="seconds"
+            :size="cardSize"
+            show-value
+            :thickness="thick"
+            :color="color"
+            :center-color="center"
+            :track-color="track"
+            :square="true"
+            class="q-ma-md"
+            :bordered="false"
+            :min="0"
+            :max="60"
+          />
+          <h2 class="text-center">Secondes</h2>
+        </div>
+      </div>
+    </div>
+
+    <!-- Mobile -->
+    <h2 class="text-center text-bold">15 JUILLET 2023</h2>
+
+    <div class="row lt-md">
       <div class="col" ref="myCol">
         <q-circular-progress
           :value="month"
@@ -17,7 +111,7 @@
           :min="0"
           :max="12"
         />
-        <h2 class="text-center">Mois</h2>
+        <h6 class="text-center">Mois</h6>
       </div>
       <div class="col">
         <q-circular-progress
@@ -34,59 +128,61 @@
           :min="0"
           :max="500"
         />
-        <h2 class="text-center">Jours</h2>
+        <h6 class="text-center">Jours</h6>
       </div>
-      <div class="col">
-        <q-circular-progress
-          :value="hours"
-          :size="cardSize"
-          show-value
-          :thickness="thick"
-          :color="color"
-          :center-color="center"
-          :track-color="track"
-          :square="true"
-          class="q-ma-md"
-          :bordered="false"
-          :min="0"
-          :max="24"
-        />
-        <h2 class="text-center">Heures</h2>
-      </div>
-      <div class="col">
-        <q-circular-progress
-          :value="minutes"
-          :size="cardSize"
-          show-value
-          :thickness="thick"
-          :color="color"
-          :center-color="center"
-          :track-color="track"
-          :square="true"
-          class="q-ma-md"
-          :bordered="false"
-          :min="0"
-          :max="60"
-        />
-        <h2 class="text-center">Minutes</h2>
-      </div>
-      <div class="col">
-        <q-circular-progress
-          :value="seconds"
-          :size="cardSize"
-          show-value
-          :thickness="thick"
-          :color="color"
-          :center-color="center"
-          :track-color="track"
-          :square="true"
-          class="q-ma-md"
-          :bordered="false"
-          :min="0"
-          :max="60"
-        />
-        <h2 class="text-center">Secondes</h2>
-      </div>
+    </div>
+    <div class="row">
+      <div class="col" ref="hourCol">
+          <q-circular-progress
+            :value="hours"
+            :size="cardSizeMobile"
+            show-value
+            :thickness="thick"
+            :color="color"
+            :center-color="center"
+            :track-color="track"
+            :square="true"
+            class="q-ma-md"
+            :bordered="false"
+            :min="0"
+            :max="24"
+          />
+          <h6 class="text-center">Heures</h6>
+        </div>
+        <div class="col">
+          <q-circular-progress
+            :value="minutes"
+            :size="cardSizeMobile"
+            show-value
+            :thickness="thick"
+            :color="color"
+            :center-color="center"
+            :track-color="track"
+            :square="true"
+            class="q-ma-md"
+            :bordered="false"
+            :min="0"
+            :max="60"
+          />
+          <h6 class="text-center">Minutes</h6>
+        </div>
+        <div class="col">
+          <q-circular-progress
+            :value="seconds"
+            :size="cardSizeMobile"
+            show-value
+            :thickness="thick"
+            :color="color"
+            :center-color="center"
+            :track-color="track"
+            :square="true"
+            class="q-ma-md"
+            :bordered="false"
+            :min="0"
+            :max="60"
+          />
+          <h6 class="text-center">Secondes</h6>
+        </div>
     </div>
   </div>
 </template>
@@ -131,6 +227,7 @@ export default {
   },
   mounted() {
     this.cardSize = this.$refs.myCol.offsetWidth - 50 + "px";
+    this.cardSizeMobile = this.$refs.hourCol.offsetWidth - 50 + "px";
     setInterval(() => {
       let endDate = new Date(2023, 6, 15),
         now = new Date(),
@@ -142,10 +239,19 @@ export default {
       this.days = dateDiff.day;
       this.month = dateDiff.month;
     }, 500);
+
+    let thos = this;
+    window.addEventListener("resize", () => {
+      thos.cardSize = thos.$refs.myCol.offsetWidth - 50 + "px";
+      thos.cardSizeMobile = thos.$refs.hourCol.offsetWidth - 50 + "px";
+    });
   },
   data() {
     return {
+      // Taille des cercles pour desktop (et Mois, Jours en mobile)
       cardSize: "0",
+      // Taille des cercles pour mobile (Heure, Minutes, Secondes)
+      cardSizeMobile: "0",
       month: 0,
       days: 0,
       hours: 0,
